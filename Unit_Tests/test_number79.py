7bad0ed423e1f87c7e1608ed4d959ffb38569b8e
Fedor <rva.pva.85@bk.ru>
2025-04-17 11:11:47 +0300

    Full test from number 7.9 and add unit test from 7.9

--- Unit_Tests/test_number79.py
+++ Unit_Tests/test_number79.py
@@ -4,15 +4,19 @@ from number79 import MyQueue
 class Test_number3(unittest.TestCase):
 
     def test_positive1(self):
-        A = MyQueue([5,10,20,None,None,None,None,None,None,None])
+        A = MyQueue()
+        A.enqueue(10)
+        A.enqueue(5)
         p1 = A.dequeue()
         p2 = A.dequeue()
         p3 = A.dequeue()
         p = f"{p1}{p2}{p3}"
-        self.assertEqual(p, ('51020'))
+        self.assertEqual(p, ('105None'))
 
     def test_positive2(self):
-        A = MyQueue([55, 120, 20, 67, 1, 3, None, None, None, None], 5,-1)
+        A = MyQueue()
+        A.enqueue(100)
+        A.enqueue(15)
         p1 = A.dequeue()
         p2 = A.dequeue()
         p3 = A.dequeue()
@@ -20,39 +24,43 @@ class Test_number3(unittest.TestCase):
         p4 = A.dequeue()
         p5 = A.dequeue()
         p = f"{p1}{p2}{p3}{p4}{p5}"
-        self.assertEqual(p, ('5512020671'))
+        self.assertEqual(p, ('10015None100None'))
 
-    def test_granical(self):
-        A = MyQueue([55, 120, 20, 67, 1, 3, 5, 10, 30, 50],9,-1)
+    def test_granical1(self):
+        A = MyQueue()
         p1 = A.enqueue(100)
         p2 = A.enqueue(200)
-        A.dequeue()
-        print(A.mas, A.head)
         p3 = A.enqueue(300)
         p4 = A.enqueue(500)
-        p = f"{p1}{p2}{p3}{p4}"
-        self.assertEqual(p, ('FalseFalseTrueFalse'))
+        p5 = A.enqueue(500)
+        p6 = A.enqueue(500)
+        p7 = A.enqueue(500)
+        p8 = A.enqueue(500)
+        p9 = A.enqueue(500)
+        p10 = A.enqueue(500)
+        p11 = A.enqueue(500)
+        p = f"{p1}{p2}{p3}{p4}{p5}{p6}{p7}{p8}{p9}{p10}{p11}"
+        self.assertEqual(p, ('TrueTrueTrueTrueTrueTrueTrueTrueTrueTrueFalse'))
 
     def test_granical2(self):
-        A = MyQueue([55, 120, 20, 67, 1, 3, 5, 10, 30, 50],9,-1)
-        print(A.head, A.tail)
+        A = MyQueue()
         p1 = A.enqueue(100)
         p2 = A.enqueue(200)
-        A.dequeue()
         p3 = A.enqueue(300)
         p4 = A.enqueue(500)
         p5 = A.isEmpty()
-        p = f"{p1}{p2}{p3}{p4}{p5}"
-        self.assertEqual(p, ('FalseFalseFalseFalseFalse'))
+        p6 = A.dequeue()
+        p = f"{p1}{p2}{p3}{p4}{p5}{p6}"
+        self.assertEqual(p, ('TrueTrueTrueTrueFalse100'))
 
     def test_special1(self):
-        A = MyQueue([55, 120, 20, 67, 1, 3, 5, 10, 30, 50],9,-1)
-        p1 = A.pop()
-        p2 = A.pop()
-        p3 = A.pop()
+        A = MyQueue()
+        p1 = A.dequeue()
+        p2 = A.dequeue()
+        p3 = A.dequeue()
         p4 = A.isEmpty()
-        A.push(100)
-        p5 = A.pop()
+        A.enqueue(100)
+        p5 = A.dequeue()
         p6 = A.isEmpty()
         p = f"{p1}{p2}{p3}{p4}{p5}{p6}"
-        self.assertEqual(p, ('20105True100True'))
+        self.assertEqual(p, ('NoneNoneNoneTrue100True'))
--- number79.py
+++ number79.py
@@ -1,66 +1,53 @@
 class MyQueue:
     # конструктор, инициализирующий все необходимые поля необходимыми значениями
-    def __init__(self, mas=[None]*10, head=0, tail=-1):
+    def __init__(self):
         self.boolean = False
-        self.mas = mas
-        if mas[0] == None:
-            self.head = 0
-            self.tail = -1
-        else:
-            self.head = head
-            self.tail = tail
+        self.mas = [None] * 10
+        self.head = 0
+        self.tail = -1
 
     # метод добавления элемента в очередь
     def enqueue(self, x):
-        if self.mas[self.head + 1] != None:
+        if self.boolean:
             return False
-        self.mas[self.head] = x
-        self.head += 1
+        if self.tail == -1:
+            self.tail = 0
+        else:
+            self.tail = (self.tail + 1) % 10
+        self.mas[self.tail] = x
+        if self.head == (self.tail + 1) % 10:
+            self.boolean = True
+        else:
+            self.boolean = False
         return True
 
     # метод удаления элемента из очереди
     def dequeue(self):
-        if self.mas[self.tail + 1] == None:
+        if self.isEmpty():
             return None
-        q = self.mas[self.tail + 1]
-        self.mas = self.mas[1:] + [None]
-        self.head -= 1
+        q = self.mas[self.head]
+        self.mas[self.head] = None
+        self.head = (self.head + 1) % 10
         return q
     # метод для определения, пуста ли очередь
     def isEmpty(self):
-        if ((self.tail + 1) % 10) == (self.head % 10) and len(self.mas) != 0:
+        if (self.head > self.tail) and (self.mas[self.head] == None) and (self.mas[self.tail] == None):
             return True
         return False
 # Этот код менять не нужно. При корректной реализации класса MyQueue он должен выдать корректный результат
 # Раскомментируйте этот код, когда перестанете получать сообщения об ошибках
 if __name__ == '__main__':
     A = MyQueue()
-    print(A.isEmpty())
-    A.enqueue(1)
-    A.enqueue(2)
-    print(A.dequeue())
-    A.enqueue(3)
-    print(A.dequeue())
-    A.enqueue(4)
-    A.enqueue(5)
-    print(A.isEmpty())
-    A.enqueue(6)
-    print(A.dequeue())
-    print(A.dequeue())
-    print(A.dequeue())
-    print(A.dequeue())
-    print(A.isEmpty())
-    A.enqueue(10)
-    A.enqueue(11)
-    A.enqueue(12)
-    A.enqueue(13)
-    A.enqueue(14)
-    A.enqueue(15)
-    A.enqueue(16)
-    print(A.dequeue())
-    print(A.dequeue())
-    print(A.dequeue())
-    print(A.dequeue())
-    print(A.dequeue())
-    print(A.dequeue())
-    print(A.dequeue())
+    print(A.enqueue(1))
+    print(A.enqueue(2))
+    print(A.enqueue(3))
+    print(A.enqueue(4))
+    print(A.enqueue(5))
+    print(A.enqueue(6))
+    print(A.enqueue(10))
+    print(A.enqueue(11))
+    print(A.enqueue(12))
+    print(A.enqueue(13))
+    print(A.enqueue(14))
+    print(A.enqueue(15))
+    print(A.enqueue(16))
